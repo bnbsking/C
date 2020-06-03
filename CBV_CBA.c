64d58122187f3ebc94fe111f6daef6202f2b1a0a
Bobo <bnbsking@gmail.com>
2020-06-03 13:55:54 +0800

    Update CBV_CBA.c

--- CBV_CBA.c
+++ CBV_CBA.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-//cbv: call by value 			(非指標變數，複製獨立一份)
+//cbv: call by value 		(非指標變數，複製獨立一份)
 //cba: call by address/pointer 	(指標變數，建立捷徑)
 
 //a add 1 call by value
-int cbv_int(int x){         //int x = a, x為獨立一份
+int cbv_int(int x){         	//int x = a, x為獨立一份
 	x = x+1;                //改x不影響a
 	return x;               //return改過的x(要用a去接)
 }
 
 //a add 1 call by address
-void cba_int(int* ptr){     //int* ptr = &a, ptr為a之地址, *ptr即a
+void cba_int(int* ptr){     	//int* ptr = &a, ptr為a之地址, *ptr即a
 	*ptr = *ptr + 1;        //改*ptr就是改a, 所以不須回傳
 }
 
@@ -20,7 +20,7 @@ void cba_arr(int* ptr, int n){	//int* ptr = arr, ptr即arr即陣列第0個元素
 	for(i=0; i<n; i++) ptr[i] = ptr[i]+1; //改*ptr就是改*arr, 因此不須回傳
 }
 
-int ga = 3;					//global variable
+int ga = 3;			//global variable
 //global a add 1 call by none
 void cbn_int(){
 	ga = ga+1;
@@ -35,7 +35,7 @@ void cbn_arr(){
 
 int main()
 {
-	int a=5;				//獨立於global a
+	int a=5;		//獨立於global a
 	a = cbv_int(a);
 	printf("%d \n", a);
 
@@ -54,5 +54,5 @@ int main()
 	cbn_arr();
 	for(i=0; i<3; i++) printf("%d ", garr[i]); printf("\n");
 
-    return 0;
+    	return 0;
 }
